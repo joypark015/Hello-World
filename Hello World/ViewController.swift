a6e643d9e7073fb794a152b0faee099392ce2c5e
Park <jcpfmc@ENGR-PARKCHANW1.local>
2019-01-31 15:18:16 -0600

    First commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Park, Joy (MU-Student) on 1/31/19.
+//  Copyright © 2019 Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
